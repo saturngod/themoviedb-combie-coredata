893b9a335c95d8ac7c7ed2c29a0a54257d1ef215
saturngod <saturngod@gmail.com>
2022-10-16 15:23:51 +0630

    chage the API key that gave in PDF file

--- TheMoviesDB/UseCases/ApiConstants.swift
+++ TheMoviesDB/UseCases/ApiConstants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct ApiConstants {
-    static let apiKey = "181af7fcab50e40fabe2d10cc8b90e37"
+    static let apiKey = "0e7274f05c36db12cbe71d9ab0393d47"
     static let baseUrl = URL(string: "https://api.themoviedb.org/3")!
     static let originalImageUrl = URL(string: "https://image.tmdb.org/t/p/original")!
     static let smallImageUrl = URL(string: "https://image.tmdb.org/t/p/w300")!
